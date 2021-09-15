b5a66349ab0208969dc4f1f6016251a11180fc64
liluacrobat <lilu.acrobat@gmail.com>
2021-09-15 13:04:21 -0400

    Delete MicrobiomRoutain_v0.1 directory

--- MicrobiomRoutain_v0.1/src/catLabel.m
+++
@@ -1,5 +0,0 @@
-function x = catLabel(meta,idx)
-% Categorize string labels into 1,2,3,...
-x.raw = table2array(meta(:,idx));
-[x.legend, ~, x.y] = unique(x.raw);
-end
--- MicrobiomRoutain_v0.1/src/selBeta.m
+++
@@ -1,6 +0,0 @@
-function y = selBeta(x,sel)
-y = x;
-y.bc = x.bc(sel,sel);
-y.jc = x.jc(sel,sel);
-y.thetayc = x.thetayc(sel,sel);
-end
