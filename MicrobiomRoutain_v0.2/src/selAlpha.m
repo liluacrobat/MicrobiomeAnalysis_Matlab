e344bc10fba699c1f1082968c722679b486db606
liluacrobat <lilu.acrobat@gmail.com>
2021-09-15 14:11:05 -0400

    Add files via upload

--- MicrobiomRoutain_v0.2/src/selAlpha.m
+++ MicrobiomRoutain_v0.2/src/selAlpha.m
@@ -1,11 +1,20 @@
 function y = selAlpha(x,sel)
-if isempty(x)
-    y=x;
+
+y = x;
+if isempty(x.ob_otu)
+    y.ob_otu = [];
 else
-    y = x;
     y.ob_otu = x.ob_otu(sel);
+end
+if isempty(x.shannon)
+    y.shannon = [];
+else
     y.shannon = x.shannon(sel);
-    y.simposon = x.simposon(sel);
+end
+y.simposon = x.simposon(sel);
+if isempty(x.chao1)
+    y.chao1 = [];
+else
     y.chao1 = x.chao1(sel);
 end
 end
