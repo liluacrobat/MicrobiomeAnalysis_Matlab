fb5ea0626d18707858ab38fa5a43427010676580
liluacrobat <lilu.acrobat@gmail.com>
2021-09-15 14:12:46 -0400

    Add files via upload

--- MicrobiomRoutain_v0.2/src/selBeta.m
+++ MicrobiomRoutain_v0.2/src/selBeta.m
@@ -1,11 +1,18 @@
 function y = selBeta(x,sel)
-if isempty(x)
-    y = x;
+y = x;
+if isempty(x.bc)
+    y.bc = [];
 else
-    
-    y = x;
     y.bc = x.bc(sel,sel);
+end
+if isempty(x.jc)
+    y.jc = [];
+else
     y.jc = x.jc(sel,sel);
+end
+if isempty(x.thetayc)
+    y.thetayc = [];
+else
     y.thetayc = x.thetayc(sel,sel);
 end
 end
