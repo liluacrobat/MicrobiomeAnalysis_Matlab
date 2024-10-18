1768b8da0e332dd56ef0d64db6c7710be34931c4
liluacrobat <lilu.acrobat@gmail.com>
2024-10-18 15:51:20 -0400

    Delete MicrobiomRoutain_v0.2 directory

--- MicrobiomRoutain_v0.2/src/EsSimpson.R
+++
@@ -1,8 +0,0 @@
-rm(list=ls())
-require(R.matlab)
-require(vegan)
-D<-readMat("simpson_temp.mat");
-X<-D$X;
-# estimate Chao1 index
-rich <-diversity(X, index = "simpson", MARGIN = 2)
-writeMat("simpson_index.mat",rich=rich);
